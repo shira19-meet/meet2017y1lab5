69fefaa7d9694733567e5107bbb7d5a0de2feac7
shira19-meet <shira19@meet.mit.edu>
2017-07-30 11:47:31 +0300

    Stopping here

--- fun1.py
+++ fun1.py
@@ -1,8 +1,8 @@
 
 def add_numbers(start, end):
     c = 0
-    for number in range (start, end+1):
-        print(number)
+    for number in range (start, end+1,2):
+#        print(number)
         c = c+number
     return(c)
 
--- fun2.py
+++ fun2.py
@@ -1,10 +1,31 @@
-def draw_1d(number):
-    
-    draw = "*"
-    print(draw*number)
-
-    
-draw_1d(20)
 
 
+def draw_1d(n,c):
+    for loop in range (5):
+        print(c*n)
 
+def draw_2d(n,m,char1):
+    for loop in range (n):
+        print(m*char1)
+
+def special_draw_2d (n,m,border,fill):
+    print(border*m)
+    for loop in range (n-2):
+         print(border+fill*(m-2)+border)
+    print(border*m)
+
+
+
+
+
+
+def fib(5):
+    cur = 1
+    old = 1
+    i = 1
+    while (i < n):
+        cur, old, i = cur+old, cur, i+1
+    return cur
+
+for i in range(10):
+    print(fib(i))
